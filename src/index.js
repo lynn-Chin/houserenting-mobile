113c219690488855459b3d1670defa62de73ac0b
lynn_Q <lynnching@outlook.com>
2020-10-13 12:29:26 +0800

    02-全局样式初始化与样式变量

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+// import global style
+import './styles/global.scss'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
