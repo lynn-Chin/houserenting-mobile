c6a51d6f9ab0b9ee8612027bb1f9cfdbb5e68690
lynn_Q <lynnching@outlook.com>
2020-10-15 11:29:18 +0800

    09-引入axios

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 // import global style
 import './styles/global.scss'
+import Axios from 'axios'
+
+React.Component.prototype.$axios = Axios.create({
+  baseURL: 'http://157.122.54.189:9060'
+})
 
 ReactDOM.render(
     <App />,
